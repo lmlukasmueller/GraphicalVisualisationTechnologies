857ad97314566a66192022aed080f38da9ba0252
lmlukasmueller <95484954+lmlukasmueller@users.noreply.github.com>
2022-12-04 15:11:02 +0100

    Update Script.js

--- EA9/Script.js
+++ EA9/Script.js
@@ -253,9 +253,9 @@ var app = (function() {
         });
  
         createModel("plane", fs, [ 1, 1, 1, 1 ], [ 0, 0, 0, 0 ], [ 0, 0, 0,
-                0 ], [ 1, 1, 1, 1 ], mGrey, "textures/plane.jpg");
+                0 ], [ 1, 1, 1, 1 ], mGrey, "textures/torus.jpg");
 		createModel("torus", fs, [ 1, 1, 1, 1 ], [ 0, 2, 0 ], [ 0, 0, 0 ],
-                [ 2, 2, 2 ], mWhite, "textures/torus.jpg");
+                [ 2, 2, 2 ], mWhite, "textures/plane.jpg");
  
         // Select one model that can be manipulated interactively by user.
         interactiveModel = models[0];
